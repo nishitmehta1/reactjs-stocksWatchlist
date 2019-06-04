4741c2c63f049f8f84d6296bc6e1d2069b7a6a89
Nishit Mehta <nishit.mehta1@gmail.com>
2019-06-04 18:49:17 -0500

    Updates

---
+++ precache-manifest.740a6ae5a77be6ef3730eef3580ac074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c907c5ec13ef533297ac99ca85a4f24f",
+    "url": "/reactjs-stocksWatchlist/index.html"
+  },
+  {
+    "revision": "d4736b9b41b5266fb833",
+    "url": "/reactjs-stocksWatchlist/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4308b29cd90d5567e402",
+    "url": "/reactjs-stocksWatchlist/static/css/main.c68e1c3b.chunk.css"
+  },
+  {
+    "revision": "d4736b9b41b5266fb833",
+    "url": "/reactjs-stocksWatchlist/static/js/2.caef217f.chunk.js"
+  },
+  {
+    "revision": "4308b29cd90d5567e402",
+    "url": "/reactjs-stocksWatchlist/static/js/main.b044fcc0.chunk.js"
+  },
+  {
+    "revision": "364956ca910cd5dca529",
+    "url": "/reactjs-stocksWatchlist/static/js/runtime~main.048085c3.js"
+  }
+]);
